3bc1919a16f02a1bac6f8bcc3e98030ad08e5c4b
Antoine R. Dumont <antoine.romain.dumont@gmail.com>
2015-07-20 09:17:51 +0200

    Define a minor mode

--- irc-pack.el
+++ irc-pack.el
@@ -37,9 +37,9 @@ This could be a plain authinfo file too.")
   "Compute the irc SERVER uri from the SERVER and the PORT."
   (format "%s:%s" server port))
 
-(defun irc-pack/log (str)
-  "Log the string STR in the mini-buffer."
-  (message "irc-pack - %s" str))
+(defun irc-pack/log (&rest args)
+  "Log the message ARGS in the mini-buffer."
+  (apply #'message (format "IRC Pack - %s" (car args)) (cdr args)))
 
 (defun irc-pack/setup-possible-p (creds-file)
   "Check if the setup is possible.
@@ -69,18 +69,17 @@ If it does return such entry, nil otherwise."
     (erc-track-mode t)
     ;; some specific setup
     (setq ;; erc-track-exclude-types '("JOIN" "NICK" "PART" "QUIT" "MODE"
-          ;;                           "324" "329" "332" "333" "353" "477")
-          ;; don't show any of this
-          ;; erc-hide-list '("JOIN" "PART" "QUIT" "NICK")
-          erc-prompt-for-nickserv-password nil
-          erc-nickserv-passwords `((freenode ((,login . ,password))))
-          ;; keep the credentials
-          irc-pack/login login
-          irc-pack/password password
-          irc-pack/fullname fullname)
+     ;;                           "324" "329" "332" "333" "353" "477")
+     ;; don't show any of this
+     ;; erc-hide-list '("JOIN" "PART" "QUIT" "NICK")
+     erc-prompt-for-nickserv-password nil
+     erc-nickserv-passwords `((freenode ((,login . ,password))))
+     ;; keep the credentials
+     irc-pack/login login
+     irc-pack/password password
+     irc-pack/fullname fullname)
     ;; add keybindings
-    (global-set-key (kbd "C-c c i") 'irc-pack/erc-start-or-switch)
-    (irc-pack/log "irc-pack setup done!")))
+    (irc-pack/log "Setup done!")))
 
 ;; ===================== setup routine
 
@@ -92,6 +91,29 @@ If it does return such entry, nil otherwise."
     (irc-pack/log (concat "You need to setup the credentials file " *IRC-PACK-CREDENTIALS-FILE* " for this to work.\n"
                           "Here is the needed content to setup to your need into '" *IRC-PACK-CREDENTIALS-FILE* "':\n"
                           "machine irc login <your-login> password <your-password> fullname <your-full-name-in-quote-if-need-be>"))))
+
+(defvar irc-pack-mode-map
+  (let ((map (make-sparse-keymap)))
+    (define-key map (kbd "C-c i l") 'irc-pack/load-pack!)
+    (define-key map (kbd "C-c i c") 'irc-pack/erc-start-or-switch)
+    (define-key map (kbd "C-c i d") 'erc-quit-server)
+    map)
+  "Keymap for git-pack mode.")
+
+(define-minor-mode irc-pack-mode
+  "Minor mode to consolidate irc-pack extensions.
+
+\\{irc-pack-mode-map}"
+  :lighter " IP"
+  :keymap irc-pack-mode-map)
+
+(define-globalized-minor-mode global-irc-pack-mode irc-pack-mode irc-pack-on)
+
+(defun irc-pack-on ()
+  "Turn on `irc-pack-mode'."
+  (irc-pack-mode +1))
+
+(global-irc-pack-mode)
 
 (provide 'irc-pack)
 ;;; irc-pack.el ends here
